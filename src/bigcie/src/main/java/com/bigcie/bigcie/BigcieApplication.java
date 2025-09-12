331010035da3a2243fa87ea195066fcc5f483765
antholim <anthonylim459@hotmail.com>
2025-09-12 10:57:48 -0400

    init commit

---
+++ src/bigcie/src/main/java/com/bigcie/bigcie/BigcieApplication.java
@@ -0,0 +1,13 @@
+package com.bigcie.bigcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigcieApplication.class, args);
+	}
+
+}
---
+++ src/bigcie/src/test/java/com/bigcie/bigcie/BigcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcie.bigcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigcieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
