2aa3c742e6af287e273f263e028feb9e8fdec002
antholim <139510082+antholim@users.noreply.github.com>
2025-11-18 18:19:57 -0500

    my profile endpoint for tier

--- src/bigcie/src/main/java/bigcie/bigcie/services/interfaces/IUserService.java
+++ src/bigcie/src/main/java/bigcie/bigcie/services/interfaces/IUserService.java
@@ -1,5 +1,6 @@
 package bigcie.bigcie.services.interfaces;
 
+import bigcie.bigcie.dtos.MyProfileInformation.UserProfileInformationDTO;
 import bigcie.bigcie.entities.User;
 
 import java.util.UUID;
@@ -7,4 +8,5 @@ import java.util.UUID;
 public interface IUserService {
     User getUserByUUID(UUID userId);
     User updateUser(User user);
+    UserProfileInformationDTO getUserProfileInformation(UUID userId);
 }
