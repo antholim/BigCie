be393df89706d52d9de0ce6a76020b8a1bf8039d
antholim <139510082+antholim@users.noreply.github.com>
2025-11-23 21:09:13 -0500

    remove function for script on start

--- src/bigcie/src/main/java/bigcie/bigcie/scripts/Script.java
+++ src/bigcie/src/main/java/bigcie/bigcie/scripts/Script.java
@@ -49,7 +49,7 @@ public class Script implements CommandLineRunner {
 //        reservationPopulate();
 //        defaulttobronze();
 //        bronzeToSilver();
-        silverToGold();
+//        silverToGold();
     }
 
     private void recountAllStations() {
